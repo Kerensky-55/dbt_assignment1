f1470c3ef650f7bd129c038ff08c43d3c8ebd6f5
Ankur Falari <Kerenskyfalari55@gmail.com>
2025-02-19 05:16:45 +0000

    fix dim_customer no duplicate values anymore

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,8 +1,21 @@
-with customers as (
+with ranked_customers as (
     select
-        distinct customer_id,
+        customer_id,
+        country,
+        invoicedate,
+        row_number() over (partition by customer_id order by invoicedate desc) as row_num
+    from
+        {{ ref('stg_raw_retail_schema__raw_retail_data') }}
+),
+final as (
+    select
+        customer_id,
         country
-    from {{ ref('stg_raw_retail_schema__raw_retail_data') }}
+    from
+        ranked_customers
+    where
+        row_num = 1
+    order by customer_id
 )
 
-select * from customers
+select * from final
