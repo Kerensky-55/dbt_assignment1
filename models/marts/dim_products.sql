df80c47b0a0df41849ca448e0c536aadb8a5ca97
Ankur Falari <Kerenskyfalari55@gmail.com>
2025-02-19 05:28:39 +0000

    fixed dim_products removed duplicate stock code

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,9 +1,22 @@
-with products as (
-    select
-        distinct stock_code,
+WITH ranked_products AS (
+    SELECT
+        stock_code,
+        description,
+        unit_price,
+        invoicedate,
+        ROW_NUMBER() OVER (PARTITION BY stock_code ORDER BY invoicedate DESC) AS row_num
+    FROM
+        {{ ref('stg_raw_retail_schema__raw_retail_data') }}
+),
+final as (
+    SELECT
+        stock_code,
         description,
         unit_price
-    from {{ ref('stg_raw_retail_schema__raw_retail_data') }}
+    FROM
+        ranked_products
+    WHERE
+        row_num = 1
 )
 
-select * from products
+select * from final
