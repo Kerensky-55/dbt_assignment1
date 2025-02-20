71f4553f2e66cdb6ef936cc51c2487ab876f68bd
Ankur Falari <Kerenskyfalari55@gmail.com>
2025-02-20 10:20:09 +0000

    changed materializations

--- models/marts/dimensions/dim_products.sql
+++ models/marts/dimensions/dim_products.sql
@@ -1,3 +1,8 @@
+{{ config(
+    cluster_by=['stock_code']
+    )
+}}
+
 with latest_unit_price as (
     select
         stock_code,
@@ -10,7 +15,8 @@ with latest_unit_price as (
 total_sold as (
     select
         stock_code,
-        sum(quantity) as total_quantity_sold
+        sum(quantity) as total_quantity_sold,
+        round(sum(revenue), 2) as total_revenue
     from {{ ref("fct_sales") }}
     group by stock_code
 ),
@@ -19,7 +25,8 @@ products as (
         l.stock_code,
         l.description,
         l.unit_price as latest_unit_price,
-        t.total_quantity_sold
+        t.total_quantity_sold,
+        t.total_revenue
     from latest_unit_price l
     left join total_sold t on l.stock_code = t.stock_code
     where l.row_num = 1
--- models/marts/facts/fct_rfm.sql
+++ models/marts/facts/fct_rfm.sql
@@ -1,3 +1,9 @@
+{{ config(
+    materialized='incremental',
+    unique_key=['customer_id'],
+    cluster_by=['customer_id']
+) }}
+
 with customer_spending as (
     select
         customer_id,
@@ -5,15 +11,18 @@ with customer_spending as (
         total_orders
     from {{ ref("int__customer_spendings") }}
 ),
+
 latest_purchase as (
     select
         customer_id,
         latest_purchase
     from {{ ref("int__customer_lifetime") }}
 ),
+
 current_date as (
     select max(invoice_date) as today from {{ ref("fct_sales") }}
 ),
+
 rfm as (
     select
         c.customer_id,
@@ -23,15 +32,9 @@ rfm as (
     from customer_spending c
     join latest_purchase l on c.customer_id = l.customer_id
     join current_date cd on 1=1
-),
-
-final_rfm as (
-    select
-        customer_id,
-        recency,
-        frequency,
-        monetary_value,
-    from rfm
+    {% if is_incremental() %}
+    where l.latest_purchase > (select max(latest_purchase) from {{ this }})
+    {% endif %}
 )
 
-select * from final_rfm
+select * from rfm
